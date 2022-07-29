9b9406a904f17d12dd6512dc953d70671e1b11e1
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-07-29 17:37:46 -0500

    ControllesCath

--- backend/controllers/authControllers.js
+++ backend/controllers/authControllers.js
@@ -1,19 +1,36 @@
 const { request, response } = require("express");
 
-const createAccount = (req = request, res = response) => {
-  res.json({ msg: "Crear cuenta" });
-};
-
-const loginAccount = (req = request, res = response) => {
+const createAccount = async (req = request, res = response) => {
   try {
-    res.json({ msg: "Login cuenta" });
+    res.json({ msg: "Crear cuenta" });
   } catch (error) {
-    console.log(error);
+    console.log(error.message);
+    res
+      .status(500)
+      .json({ Error: true, msg: `Algo no salio mal ${error.message}` });
   }
 };
 
-const validateToken = (req = request, res = response) => {
-  res.json({ msg: "validar secion" });
+const loginAccount = async (req = request, res = response) => {
+  try {
+    res.json({ msg: "Login cuenta" });
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(500)
+      .json({ Error: true, msg: `Algo no salio mal ${error.message}` });
+  }
+};
+
+const validateToken = async (req = request, res = response) => {
+  try {
+    res.json({ msg: "validar secion" });
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(500)
+      .json({ Error: true, msg: `Algo no salio mal ${error.message}` });
+  }
 };
 
 module.exports = {
--- backend/controllers/createTodoControllers.js
+++ backend/controllers/createTodoControllers.js
@@ -1,20 +1,47 @@
 const { request, response } = require("express");
-const { model } = require("mongoose");
 
-const createTodo = (req = request, res = response) => {
-  res.json({ msg: "Todo" });
+const createTodo = async (req = request, res = response) => {
+  try {
+    res.json({ msg: "Todo" });
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(500)
+      .json({ Error: true, msg: `Algo no salio mal ${error.message}` });
+  }
 };
 
-const deleteTodo = (req = request, res = response) => {
-  res.json({ msg: "deleteTodo" });
+const deleteTodo = async (req = request, res = response) => {
+  try {
+    res.json({ msg: "deleteTodo" });
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(500)
+      .json({ Error: true, msg: `Algo no salio mal ${error.message}` });
+  }
 };
 
-const updateTodo = (req = request, res = response) => {
-  res.json({ msg: "updateTodo" });
+const updateTodo = async (req = request, res = response) => {
+  try {
+    res.json({ msg: "updateTodo" });
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(500)
+      .json({ Error: true, msg: `Algo no salio mal ${error.message}` });
+  }
 };
 
-const getTodos = (req = request, res = response) => {
-  res.json({ msg: "getTodos" });
+const getTodos = async (req = request, res = response) => {
+  try {
+    res.json({ msg: "getTodos" });
+  } catch (error) {
+    console.log(error.message);
+    res
+      .status(500)
+      .json({ Error: true, msg: `Algo no salio mal ${error.message}` });
+  }
 };
 
 module.exports = {
