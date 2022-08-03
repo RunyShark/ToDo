1de0cd03dc5cb9d5e228782c20167bbb352a2b5f
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-08-03 10:52:26 -0500

    solutions

--- client/src/api/todoAPI.ts
+++ client/src/api/todoAPI.ts
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-import { getEnvVariables } from "../helpers";
-const { VITE_API_URL } = getEnvVariables();
+// import { getEnvVariables } from "../helpers";
+// const { VITE_API_URL } = getEnvVariables();
 
 const todoAPI = axios.create({
-  baseURL: VITE_API_URL,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 todoAPI.interceptors.request.use((config: any) => {
